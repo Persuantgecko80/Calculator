13e4629b44ccf2440a960854e416147de54564f8
Yosef Lachman <46474203+Persuantgecko80@users.noreply.github.com>
2023-09-26 17:19:50 +0300

    trying reworks to get the html and css to match up and make it look good

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ function operate(operator, a, b) {
 
 
 function populateDisplay(value) {
+    console.log("populateDisplay called with:", value);
     if (isEqualsPressed) {
       firstNumber = "";
       secondNumber = "";
@@ -48,15 +49,22 @@ function populateDisplay(value) {
     }
   
     if ("0123456789".includes(value)) {
-      if (isOperatorSelected) {
-        secondNumber += value;
-      } else {
-        firstNumber += value;
-      }
-    } else if ("+-×÷".includes(value)) {
-      operator = value;
-      isOperatorSelected = true;
-    } else if (value === "=") {
+        if (isOperatorSelected) {
+          secondNumber += value;
+        } else {
+          firstNumber += value;
+        }
+        displayValue += value;
+      } else if ("+-×÷".includes(value)) {
+        if (firstNumber && operator && secondNumber) {
+          let result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
+          firstNumber = result.toString();
+          secondNumber = "";
+        }
+        operator = value;
+        isOperatorSelected = true;
+        displayValue += value;
+      } else if (value === "=") {
       isEqualsPressed = true;
       if (firstNumber && operator && secondNumber) {
         displayValue = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber)).toString();
@@ -71,11 +79,13 @@ function populateDisplay(value) {
   
 
 function updateDisplay() {
+    console.log("updateDisplay called with:", displayValue);
     const displayElement = document.querySelector(".screen");
     displayElement.innerText = displayValue;
 };
 
 document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM loaded");
     document.querySelectorAll("button").forEach(function(button) {
         button.addEventListener("click", function(e) { 
             const buttonValue = e.target.innerText;
